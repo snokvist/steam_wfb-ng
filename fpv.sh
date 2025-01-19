f7ba51a9f4909b3e2f64cb8cdb3bc2a869e01bc2
snokvist <joakim.snokvist@gmail.com>
2025-01-19 15:15:46 +0100

    Update fpv.sh

--- fpv.sh
+++ fpv.sh
@@ -18,10 +18,10 @@ trap "exit 2" HUP INT QUIT TERM
 while true
 do
 #Only video
-gst-launch-1.0 udpsrc port=5600 ! queue max-size-time=1 ! application/x-rtp,payload=97, clock-rate=90000, encoding-name=H265 ! rtpjitterbuffer latency=1 ! rtph265depay ! vaapih265dec ! fpsdisplaysink video-sink=xvimagesink sync=true
+#gst-launch-1.0 udpsrc port=5600 ! queue max-size-time=1 ! application/x-rtp,payload=97, clock-rate=90000, encoding-name=H265 ! rtpjitterbuffer latency=1 ! rtph265depay ! vaapih265dec ! fpsdisplaysink video-sink=xvimagesink sync=true
 
 #Vido + Audio
-#gst-launch-1.0 udpsrc port=5600 ! tee name=t t. ! queue max-size-time=1 ! application/x-rtp,payload=97, clock-rate=90000, encoding-name=H265 ! rtpjitterbuffer latency=1 ! rtph265depay ! vaapih265dec ! xvimagesink sync=false async=false t. ! queue leaky=1 ! application/x-rtp, payload=98, encoding-name=OPUS ! rtpjitterbuffer latency=1 ! rtpopusdepay ! opusdec ! audioconvert ! audioresample ! alsasink sync=false async=false
+gst-launch-1.0 udpsrc port=5600 ! tee name=t t. ! queue max-size-time=1 ! application/x-rtp,payload=97, clock-rate=90000, encoding-name=H265 ! rtpjitterbuffer latency=1 ! rtph265depay ! vaapih265dec ! xvimagesink sync=false async=false t. ! queue leaky=1 ! application/x-rtp, payload=98, encoding-name=OPUS ! rtpjitterbuffer latency=1 ! rtpopusdepay ! opusdec ! audioconvert ! audioresample ! alsasink sync=false async=false
 
 echo "GST crashed with exit code:$?" >> /tmp/fpv.log
 sleep 1

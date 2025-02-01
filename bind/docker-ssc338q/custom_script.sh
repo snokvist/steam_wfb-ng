5efae62d2f2130859291b0038791baf5ffcc8f0e
snokvist <joakim.snokvist@gmail.com>
2025-02-01 20:44:15 +0100

    Add files via upload

---
+++ bind/docker-ssc338q/custom_script.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Put custom commands to be executed in this script."
+
+# Capture the output of the command.
+output=$(wfb-cli -g .broadcast.wfb_passphrase)
+
+# Check if the output is non-empty.
+if [ -n "$output" ]; then
+  echo "Passphrase retrieved: $output"
+  # Run the additional command here.
+  keypair $output
+else
+  echo "No passphrase retrieved, please check the command."
+  # Optionally run another command here.
+  cp etc/gs.key /etc/drone.key
+fi
+
+exit 0

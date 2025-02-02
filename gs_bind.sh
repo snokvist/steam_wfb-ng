01cb56de2b0d9793a6a1b4686d538a65a315f4c0
snokvist <joakim.snokvist@gmail.com>
2025-02-02 12:40:10 +0100

    Add files via upload

--- gs_bind.sh
+++ gs_bind.sh
@@ -5,6 +5,7 @@ echo "Calling wlan_init.sh"
 sleep 3
 #do stuff $2 =  bind_data_folder
 echo "Starting SEND: "
-./connect.py $2
+./connect.py --bind $2
+#./connect.py --info
 ./final_cleanup.sh
 exit 0

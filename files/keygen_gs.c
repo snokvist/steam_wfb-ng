f94fe8d5f5dd970ae70570ee78f54ad854478ba7
snokvist <joakim.snokvist@gmail.com>
2025-02-02 11:33:37 +0100

    Update keygen_gs.c

--- files/keygen_gs.c
+++ files/keygen_gs.c
@@ -1,59 +1,48 @@
-// Copyright (C) 2017 - 2024 Vasily Evseenko <svpcom@p2ptech.org>
-
-/*
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; version 3.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License along
- *   with this program; if not, write to the Free Software Foundation, Inc.,
- *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- */
-
-#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sodium.h>
 #include <string.h>
 
 int main(int argc, char **argv) {
-	unsigned char publickey[crypto_box_PUBLICKEYBYTES];
-	unsigned char secretkey[crypto_box_SECRETKEYBYTES];
-	FILE *fp;
+    unsigned char publickey[crypto_box_PUBLICKEYBYTES];
+    unsigned char secretkey[crypto_box_SECRETKEYBYTES];
+    FILE *fp;
 
-	if (argc != 2) {
-		printf("Passphrase is missing\n");
-		return 1;
-	}
+    // Ensure we have at least the passphrase; allow an optional key file path.
+    if (argc < 2 || argc > 3) {
+        fprintf(stderr, "Usage: %s <passphrase> [key_file_path]\n", argv[0]);
+        return 1;
+    }
 
-	if (sodium_init() < 0) {
-		printf("Libsodium init failed\n");
-		return 1;
-	}
+    if (sodium_init() < 0) {
+        fprintf(stderr, "Libsodium init failed\n");
+        return 1;
+    }
 
-	char seed[32];
-	strncpy(seed, argv[1], sizeof(seed));
-	printf("Using passphrase: %s\n", seed);
+    // Copy the passphrase into seed.
+    char seed[32];
+    strncpy(seed, argv[1], sizeof(seed));
+    seed[sizeof(seed) - 1] = '\0';  // Ensure null-termination
+    printf("Using passphrase: %s\n", seed);
 
-	if (crypto_box_seed_keypair(publickey, secretkey, seed) != 0) {
-		printf("Unable to generate key\n");
-		return 1;
-	}
+    if (crypto_box_seed_keypair(publickey, secretkey, seed) != 0) {
+        fprintf(stderr, "Unable to generate key\n");
+        return 1;
+    }
 
-	const char *key = "./gs.key";
-	if ((fp = fopen(key, "w")) == NULL) {
-		printf("Unable to save: %s\n", key);
-		return 1;
-	}
+    // Use the second argument if provided, otherwise use the default.
+    const char *key_path = (argc == 3) ? argv[2] : "/etc/gs.key";
 
-	fwrite(secretkey, crypto_box_SECRETKEYBYTES, 1, fp);
-	fwrite(publickey, crypto_box_PUBLICKEYBYTES, 1, fp);
-	fclose(fp);
+    if ((fp = fopen(key_path, "w")) == NULL) {
+        fprintf(stderr, "Unable to save: %s\n", key_path);
+        return 1;
+    }
 
-	printf("Key saved: %s\n", key);
+    fwrite(secretkey, crypto_box_SECRETKEYBYTES, 1, fp);
+    fwrite(publickey, crypto_box_PUBLICKEYBYTES, 1, fp);
+    fclose(fp);
 
-	return 0;
+    printf("Key saved: %s\n", key_path);
+
+    return 0;
 }
